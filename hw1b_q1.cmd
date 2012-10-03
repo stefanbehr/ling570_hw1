9fa4a824884d5acb08474647d3c5fb4e7d4b64f8
stefanbehr <sbehr1@gmail.com>
2012-10-02 18:08:11 -0700

    Fixed condor files

--- hw1b_q1.cmd
+++ hw1b_q1.cmd
@@ -1,11 +1,11 @@
-Executable = eng_tokenizer
+Executable = /opt/python-2.7.2/bin/python
 Universe = vanilla
 getenv = true
 input = test1
 output = test1.tok
 error = eng_tokenizer.error
 Log = eng_tokenizer.log
-arguments = 
+arguments = eng_tokenizer
 transfer_executable = false
 request_memory = 2*1024
 Queue
--- hw1b_q3.cmd
+++ hw1b_q3.cmd
@@ -1,11 +1,11 @@
-Executable = make_vocab
+Executable = /opt/python-2.7.2/bin/python
 Universe = vanilla
 getenv = true
 input = test1
 output = test1.voc
 error = eng_tokenizer.error
 Log = eng_tokenizer.log
-arguments = 
+arguments = make_vocab
 transfer_executable = false
 request_memory = 2*1024
 Queue
