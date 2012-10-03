ee6ddc32548fbdd9f8617413237eee09bbeb49a4
stefanbehr <sbehr1@gmail.com>
2012-10-03 15:34:22 -0700

    tweaking ec

---
+++ hw1c.cmd
@@ -0,0 +1,10 @@
+Executable = hw1c.sh
+Universe = vanilla
+getenv = true
+input = jane-eyre.txt
+output = jane-eyre.top
+error = hw1c.error
+Log = hw1c.log
+transfer_executable = false
+request_memory = 2*1024
+Queue
